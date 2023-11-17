d52007a5d1674d39477c92afbe8cc8d85be65836
zergouna zebiri <zebiri.zergouna@gmail.com>
2023-11-17 09:59:23 +0100

    Ajout du fichier Gestion

---
+++ Gestion.java
@@ -0,0 +1,6 @@
+public class Gestion {
+    //Interface principale de l'application pour la gestion et l'affichage
+    public static void main(String[] agrs){
+        System.out.println("Test!");
+    }
+}
